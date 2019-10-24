473f6c0cdbf65e257c809819006efcd2bc6728fe
oike-github <chikakoike.work@gmail.com>
2019-10-25 01:03:08 +0900

    add-association
    Added associations to the following models.
    ・brand
    ・category
    ・credit
    ・evaluation
    ・item
    ・prefecture
    ・user

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :saler, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :brand
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :prefecture
+  has_many :items
+  has_many :credits
+  has_many :evaluations
 end
